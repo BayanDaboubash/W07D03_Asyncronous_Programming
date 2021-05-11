f7ae2d3cd4d93316f3d385b48cbe39a6c42dd9d7
BayanDaboubash <bayan.daboubash2000@gmail.com>
2021-05-12 00:45:56 +0300

    Q2&Q3

--- app.js
+++ app.js
@@ -4,6 +4,33 @@ const PORT = 3000;
 const fs = require("fs");
 const axios = require("axios");
 
-app.listen(PORT, () => {
 
+
+let content;
+
+const readFile = () => {
+    fs.readFile("./data.txt", (err, data) => {
+        if (err) {
+            console.log('ERR');
+        }
+        content = data.toString();
+        console.log(content);
+    });
+};
+
+const writeFile = () => {
+    fs.writeFile(
+        './data.txt',
+        `Hello Bayan`,
+        (err) => {
+          if (err) {
+            console.log(err);
+          }
+          console.log('DONE WRITE OVER THAT FILE');
+        }
+      );
+};
+
+app.listen(PORT, () => {
+    console.log("OK");    
 });
